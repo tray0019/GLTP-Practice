2d7f7ce30fdafe46b3020dd3da7bbf88487543eb
tray0019 <tray0019@algonquinlive.com>
2025-12-03 22:55:04 -0500

    initial spring boot practice

---
+++ GLTP-Practice/src/test/java/org/practice/gltppractice/GltpPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.gltppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GltpPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

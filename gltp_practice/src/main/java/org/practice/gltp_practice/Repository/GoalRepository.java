07e00a9e7d577cf732dd132b2e8fb4f1470209c1
tray0019 <tray0019@algonquinlive.com>
2025-12-05 15:39:09 -0500

    created my goal entity class and repo

---
+++ gltp_practice/src/main/java/org/practice/gltp_practice/Repository/GoalRepository.java
@@ -0,0 +1,11 @@
+package org.practice.gltp_practice.Repository;
+
+import org.practice.gltp_practice.Entity.Goal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GoalRepository extends JpaRepository<Goal, Long> {
+}
+
+

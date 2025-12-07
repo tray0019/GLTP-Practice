1b7480c575d93b23f335999fdc35582782592d67
tray0019 <tray0019@algonquinlive.com>
2025-12-07 01:23:33 -0500

    Added GoalcreateDto and GoalResponseDto

---
+++ gltp_practice/src/main/java/org/practice/gltp_practice/Dto/GoalCreateDto.java
@@ -0,0 +1,12 @@
+package org.practice.gltp_practice.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class GoalCreateDto {
+
+    @NotBlank(message = "Title is mandatory")
+    private String goalTitle;
+
+}

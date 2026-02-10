d1ab94141ed6a6d1feaf0fdd2eb5f6057a9bb712
tray0019 <tray0019@algonquinlive.com>
2026-02-10 08:34:36 -0500

    practice - added difficulty enum.

---
+++ gltp_practice/src/main/java/org/practice/gltp_practice/Entity/Difficulty.java
@@ -0,0 +1,21 @@
+package org.practice.gltp_practice.Entity;
+
+public enum Difficulty {
+
+    EASY(1), MEDIUM(2), HARD(3);
+
+    private final int value;
+
+    Difficulty(int value) {
+        this.value = value;
+    }
+
+    public int getValue(){return value;}
+
+    public static Difficulty fromValue(int value){
+        for(Difficulty d: values()){
+            if(d.getValue() == value) return d;
+        }
+        throw new IllegalCallerException("Invalid difficulty:"+value);
+    }
+}

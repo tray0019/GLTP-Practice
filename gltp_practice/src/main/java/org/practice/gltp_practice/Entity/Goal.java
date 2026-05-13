c97fc068d04b4221db1c4ff930370a0b0aff8969
tray0019 <tray0019@algonquinlive.com>
2026-05-13 17:29:38 -0400

    May/13/practice - Goal entities

--- gltp_practice/src/main/java/org/practice/gltp_practice/Entity/Goal.java
+++ gltp_practice/src/main/java/org/practice/gltp_practice/Entity/Goal.java
@@ -6,8 +6,11 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.cglib.core.Local;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity(name="Goal")
@@ -38,6 +41,48 @@ public class Goal implements Serializable {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Difficulty difficulty = Difficulty.MEDIUM;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private GoalStatus status = GoalStatus.ACTIVE;
+
+
+    @Column(nullable = false)
+    private boolean isAchievement;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @Column(name="category")
+    private String category;
+
+    @Column(name="use_case")
+    private String useCase;
+
+   @Column(nullable = false)
+    private boolean isPrivate = false;
+
+    @Column(nullable = false)
+    private boolean notesPrivate = false;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDate createdAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDate.now();
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 
